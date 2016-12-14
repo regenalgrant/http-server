a5eebd12bd8d501568f9b1cae6348e885e2f223a
regenalgrant <regenal@mac.com>
2016-12-13 16:04:05 -0800

    fix

--- src/client.py
+++ src/client.py
@@ -3,22 +3,22 @@
 import socket
 import sys
 
-if __name__ == "__main__":
-    def client(message):
-        try:
-        infos = socket.getaddressinfo('127.0.0.1', 9999)   
-        stream_infos = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
-        client.connect(stream_infos[-1])  
-        client.sendall(message.encode('utf8', 'ascii'))
+def client(message):
+        # try:
+            infos = socket.getaddressinfo('127.0.0.1', 9999)
+            stream_infos = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
+            client.connect(stream_infos[-1])
+            client.sendall(message.encode('utf8', 'ascii'))
             buffer_length = 8
             reply_complete = False
             server_message = ''
-        while not reply_complete:
-            part = client.recieved(buffer_length)
-            server_message += part.decode('utf8', 'ascii')
-        if length(part) < buffer_length:
+            while not reply_complete:
+                part = client.recieved(buffer_length)
+                server_message += part.decode('utf8', 'ascii')
+                if length(part) < buffer_length:
                     break
             client.close()
             return server_message
             client = socket.socket(*stream_info[:3])
-        print(client('YO!MTV RAPs))
+            print(client('YO!MTV RAPs'))
+# if __name__ == "__main__":
